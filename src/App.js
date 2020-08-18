2903b649392d5e95e86c7e88ce9b716f6be4d288
whitehox <oketegah@gmail.com>
2020-08-18 22:12:37 +0100

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Suspense, lazy } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { RecoilRoot } from 'recoil';
+
+const HomePage = lazy(() => import('./Pages/Home'));
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Suspense fallback="Loading...">
+      <Router>
+        <RecoilRoot>
+          <Route path="/">
+            <HomePage />
+          </Route>
+        </RecoilRoot>
+      </Router>
+    </Suspense>
   );
 }
 
