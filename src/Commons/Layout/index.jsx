35ae1d85eedc43101378b2226bd9fb89933a6f05
whitehox <oketegah@gmail.com>
2020-08-23 00:22:06 +0100

    move header to layout

--- src/Commons/Layout/index.jsx
+++ src/Commons/Layout/index.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import Header from '../Header';
 
 function Layout({ children }) {
-  return <>{children}</>;
+  return (
+    <>
+      <Header bordered={true} title="#HenryJane2020" />
+      {children}
+    </>
+  );
 }
 
 export default Layout;
