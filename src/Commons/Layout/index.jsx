04cb0778b5f782ffcf10926f5455f3347ab9f160
whitehox <oketegah@gmail.com>
2020-08-24 23:59:48 +0100

    create general content wrapper

--- src/Commons/Layout/index.jsx
+++ src/Commons/Layout/index.jsx
@@ -1,13 +1,22 @@
 import React from 'react';
 import Header from '../Header';
+import Styled from 'styled-components';
 
-function Layout({ children }) {
+function Layout({ children, fullWidth }) {
   return (
     <>
       <Header bordered={true} title="#HenryJane2020" />
-      {children}
+      <ContentWrapper fullWidth={fullWidth}>{children}</ContentWrapper>
     </>
   );
 }
+
+const ContentWrapper = Styled.section`
+  width: ${(props) => (props.fullWidth ? '100%' : '80%')};
+  margin: auto;
+  display: flex;
+  flex-direction: column;
+  padding: 40px 0; 
+`;
 
 export default Layout;
