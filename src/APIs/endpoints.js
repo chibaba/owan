50178222bafd9ea18b1966ce9693daffad374839
whitehox <oketegah@gmail.com>
2020-09-23 10:19:22 +0100

    update api

--- src/APIs/endpoints.js
+++ src/APIs/endpoints.js
@@ -4,11 +4,17 @@ const APP_URI = process.env.REACT_APP_API;
 const api = {
   //Auth API
   login: `${APP_URI}/users/v1/auths/login`,
+
   //Event API
   getEvents: `${EVENT_URI}/api/v1/event`,
   createEvent: `${EVENT_URI}/api/v1/event`,
   getEvent: (id) => `${EVENT_URI}/api/v1/event/${id}`,
+
+  //Wallet APIs
   addToWallet: `${APP_URI}/billings/wallet/fund`,
+
+  //Video API
+  startVideo: `${EVENT_URI}/api/v1/videolink`,
 };
 
 export default api;
